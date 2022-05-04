c5143ee5e6776ba73c25d27bb485c6a0c0460fe4
Yurii Voskoboinikov <voskoboinikov@Mac.local>
2022-05-04 10:14:13 +0300

    MEX-34/MEX-35/implement Open and Read methods

--- source/source.go
+++ source/source.go
@@ -19,13 +19,17 @@ import (
 
 	sdk "github.com/conduitio/conduit-connector-sdk"
 
+	"github.com/conduitio/conduit-connector-stripe/clients/http"
 	"github.com/conduitio/conduit-connector-stripe/config"
+	"github.com/conduitio/conduit-connector-stripe/source/iterator"
+	"github.com/conduitio/conduit-connector-stripe/source/position"
 )
 
 // A Source represents the source connector.
 type Source struct {
 	sdk.UnimplementedSource
-	cfg *config.Config
+	cfg      *config.Config
+	iterator *iterator.SnapshotIterator
 }
 
 // NewSource initialises a new source.
@@ -42,5 +46,37 @@ func (s *Source) Configure(ctx context.Context, cfgRaw map[string]string) error 
 
 	s.cfg = &cfg
 
+	return nil
+}
+
+// Open parses sdk.Position and initializes a Snapshot iterator.
+func (s *Source) Open(ctx context.Context, rp sdk.Position) error {
+	p, err := position.ParseSDKPosition(rp)
+	if err != nil {
+		return err
+	}
+
+	s.iterator = iterator.NewSnapshotIterator(http.NewClient(s.cfg), p)
+
+	return nil
+}
+
+// Read returns the next sdk.Record.
+func (s *Source) Read(ctx context.Context) (sdk.Record, error) {
+	r, err := s.iterator.Next()
+	if err != nil {
+		return sdk.Record{}, err
+	}
+
+	return r, nil
+}
+
+// Teardown stops the CDC iterator from running.
+func (s *Source) Teardown(ctx context.Context) error {
+	return nil
+}
+
+// Ack ...
+func (s *Source) Ack(ctx context.Context, position sdk.Position) error {
 	return nil
 }
